1b206b568c254f27686c393ad1bde51dccb5ef18
KitchenPizzeria <valomeyrick2012@hotmail.co.uk>
2020-11-01 00:59:55 +0000

    commit

--- INSPYRE/client.py
+++ INSPYRE/client.py
@@ -1,33 +1,25 @@
 import socket
 import pickle
-import time
 
 def Main():
 
 	HEADER = 40000
 	HOST = socket.gethostbyname(socket.gethostname())
-	PORT = 5005
+	PORT = 5000
 	ADDR = (HOST,PORT)
 	FORMAT = "utf-8"
 
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	s.connect(ADDR)
 
-	print("[CONNECTED] Connection Successful\n[SENDING] 'Test Message' is being sent to server ...")
-	time.sleep(2)
+	d = {"name":"Joseph","Surname":"Meyrick"}
+	msg = pickle.dumps(d)
+
+	print("[CONNECTED] Connection Successful\n[SENDING] Test dict is being sent to server ...")
 	
-
-	raw_dict = {"name": "Joseph", "Surname": "Meyrick"}
-	msg = pickle.dumps(raw_dict)
-	s.send(bytes(msg))
-	print("[SENT] Test Dictionary has been sent")
-
-	raw_data = s.recv(10240)
-	print(f"debug: receeived data is {raw_data}")
-	data = pickle.loads(raw_data)
-	#data = pickle.load(raw_data)
-	#data = raw_data.decode(FORMAT)
-	time.sleep(2)
+	s.send(msg)
+	print(f"[SENT] Dictionary has been sent")
+	data = s.recv(1024)
 	print(f"[RECEIVED] Received from server: {data}")
 
 	s.close()
